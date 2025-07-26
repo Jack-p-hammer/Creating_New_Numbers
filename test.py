62dfa04fc99e7f0b7e5f0dc12eeb87504c1c2d14
Jack-p-hammer <jonathan.hammer3@gmail.com>
2025-07-26 17:07:46 -0400

    fixing some organization problems

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-print(torch.cuda.is_available())
-print(torch.cuda.device_count())
-
-print(torch.version.cuda)     # Should print your CUDA version (e.g., '11.8')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
